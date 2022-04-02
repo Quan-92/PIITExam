7c748211a3c0b9f81d4e259927d9701683adecf5
DuyQuan <9602tranquan@gmail.com>
2022-04-02 09:34:52 +0700

    Quan

--- PIITExam/Models/Coin.cs
+++ PIITExam/Models/Coin.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,6 +9,23 @@ namespace PIITExam.Models
 {
     public class Coin
     {
-
+        [DisplayName("Id")]
+        [Required(ErrorMessage = "Please enter Id")]
+        public int Id { get; set; }
+        [DisplayName("Name")]
+        [Required(ErrorMessage = "Please enter Name")]
+        public string Name { get; set; }
+        [DisplayName("Description")]
+        [Required(ErrorMessage = "Please enter Description")]
+        public string Description { get; set; }
+        [DisplayName("CreatAt")]
+        [Required(ErrorMessage = "Please enter CreateAt")]
+        public DateTime CreateAt { get; set; }
+        [DisplayName("UpdateAt")]
+        [Required(ErrorMessage = "Please enter UpdateAt")]
+        public DateTime UpdateAt { get; set; }
+        [DisplayName("Status")]
+        [Required(ErrorMessage = "Please enter Status")]
+        public int Status { get; set; }
     }
 }
