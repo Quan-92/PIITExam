20b13aea94ceeff6b5522cf780a196f3d537fc18
DuyQuan <9602tranquan@gmail.com>
2022-04-03 18:30:47 +0700

    quan

--- PIITExam/Controllers/AdminController.cs
+++ PIITExam/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PIITExam.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,16 @@ namespace PIITExam.Controllers
     public class AdminController : Controller
     {
         // GET: Admin
-        public ActionResult Index()
+        public class MarketController : Controller
         {
-            return View();
+            public MyDbContext db = new MyDbContext();
+            public ActionResult Index(string marketName, string coinName)
+            {
+                var market = db.Markets.ToList();
+                ViewBag.Markets = new SelectList(db.Markets, "Name", "Name");
+                var coinList = db.Coins.ToList();
+                return View(coinList);
+            }
         }
     }
 }
---
+++ PIITExam/Data/MyDBContext.cs
@@ -0,0 +1,16 @@
+﻿using PIITExam.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace PIITExam.Data
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext() : base("ConnectionString") { }
+        public DbSet<Market> Markets { get; set; }
+        public DbSet<Coin> Coins { get; set; }
+    }
+}
